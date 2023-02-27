9784f344b39147dae24a0d96061e22f2a44e17db
Petr Pivoňka <petrpiv1@gmail.com>
2023-02-27 14:34:57 +0100

    Přidejte soubory projektu.

---
+++ CombineClips/CombineClipsTool.cs
@@ -0,0 +1,34 @@
+﻿using GBX.NET.Engines.Game;
+using GbxToolAPI;
+
+namespace CombineClips;
+
+public class CombineClipsTool : ITool, IHasOutput<CGameCtnMediaClip>
+{
+    private readonly IEnumerable<CGameCtnMediaClip> clips;
+
+    public CombineClipsTool(IEnumerable<CGameCtnMediaClip> clips)
+    {
+        this.clips = clips;
+    }
+
+    public CGameCtnMediaClip Produce()
+    {
+        var primaryClip = default(CGameCtnMediaClip);
+
+        foreach (var clip in clips)
+        {
+            if (primaryClip is not null)
+            {
+                foreach (var track in clip.Tracks)
+                {
+                    primaryClip.Tracks.Add(track);
+                }
+            }
+
+            primaryClip ??= clip;
+        }
+
+        return primaryClip ?? throw new Exception("No clips to combine.");
+    }
+}
